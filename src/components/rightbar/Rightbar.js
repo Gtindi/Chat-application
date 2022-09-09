7fbc2eb27599fa605a0a94c058642b896e1e0bf4
Diana-nyamai <dnyamai.dn@gmail.com>
2022-09-09 18:53:26 +0300

    update

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -3,7 +3,14 @@ import './right.css'
 
 function Rightbar() {
   return (
-    <div className='rightbar'>rightbar</div>
+    <div className='rightbar'>
+      <div className="rightbarwrapper">
+        <div className="birthdaycontainer">
+          <img src="../assets/person/p5.jpg" alt="birthday" className="birthdayimg" />
+          <span className="birthdaytext"><b>styles </b> and <b>3 other friends</b> have a birthday today</span>
+        </div>
+      </div>
+      </div>
   )
 }
 
