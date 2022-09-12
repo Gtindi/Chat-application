be200a72022246285ca9dc6d38428ef9b1ae3d4c
Diana-nyamai <dnyamai.dn@gmail.com>
2022-09-12 19:39:42 +0300

    chat application update

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -6,9 +6,20 @@ function Rightbar() {
     <div className='rightbar'>
       <div className="rightbarwrapper">
         <div className="birthdaycontainer">
-          <img src="../assets/person/p5.jpg" alt="birthday" className="birthdayimg" />
-          <span className="birthdaytext"><b>styles </b> and <b>3 other friends</b> have a birthday today</span>
+          <img src="../assets/gift.png" alt="birthday" className="birthdayimg" />
+          <span className="birthdaytext"><b>kamama</b> and <b>3 other friends</b> have a birthday today</span>
         </div>
+        <img src="../assets/ad.jpg" alt="rightad" className='rightbarad'/>
+        <h4 className="rightbartitle">Online friends</h4>
+        <ul className='rightbarfriendlist'>
+          <li className='rightbarfriend'>
+            <div className="rightbarprofileimgcontainer">
+              <img src="../assets/person/p1.jpg" alt="p1" className="rightbarprofileimg" />
+              <span className="rightbaronline"></span>
+            </div>
+            <span className="rightbarusername">mwende joe</span>
+          </li>
+        </ul>
       </div>
       </div>
   )
